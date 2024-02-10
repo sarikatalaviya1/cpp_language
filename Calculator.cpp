0d5acc21cbbccfb097f9f4b687ffe622fb56e9a9
sarikatalaviya1 <rwa1.sarika.sd@gmail.com>
2024-02-10 16:32:44 +0530

    save

---
+++ Calculator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n1,n2;
+    cout<<"Enter Number 1:";
+    cin>>n1;
+    cout<<"Enter Number 2:";
+    cin>>n2;
+    cout<<"Addition of Two Numbers:"<<n1+n2;
+    cout<<"Substraction of Two Numbers:"<<n1-n2;
+    cout<<"Multiplication of Two Numbers:"<<n1*n2;
+    cout<<"Division of Two Numbers:"<<n1/n2;
+    return 0;
+
+}
