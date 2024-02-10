548ffc2cb11a5dcba87bc42b3b1ed965a0495ee1
sarikatalaviya1 <rwa1.sarika.sd@gmail.com>
2024-02-10 16:36:30 +0530

    save

--- Calculator.cpp
+++ Calculator.cpp
@@ -7,10 +7,21 @@ int main(){
     cin>>n1;
     cout<<"Enter Number 2:";
     cin>>n2;
-    cout<<"Addition of Two Numbers:"<<n1+n2;
-    cout<<"Substraction of Two Numbers:"<<n1-n2;
-    cout<<"Multiplication of Two Numbers:"<<n1*n2;
-    cout<<"Division of Two Numbers:"<<n1/n2;
+    cout<<"Addition of Two Numbers:"<<n1+n2<<endl;
+    cout<<"Substraction of Two Numbers:"<<n1-n2<<endl;
+    cout<<"Multiplication of Two Numbers:"<<n1*n2<<endl;
+    cout<<"Division of Two Numbers:"<<n1/n2<<endl;
     return 0;
+
+// Result:
+    // Enter Number 1: 10
+    // Enter Number 2: 5
+    // Addition of Two Numbers:15
+    // Substraction of Two Numbers:5
+    // Multiplication of Two Numbers:50
+    // Division of Two Numbers:2
+
+
+
 
 }
