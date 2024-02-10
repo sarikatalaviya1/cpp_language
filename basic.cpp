6ea419d2325fbfc2b5b93b40189e26353512a80d
sarikatalaviya1 <rwa1.sarika.sd@gmail.com>
2024-02-10 16:07:00 +0530

    changes apply

--- basic.cpp
+++ basic.cpp
@@ -3,13 +3,5 @@
 int main()
 {
 
-    int a, b;
-    cout << " Enter Numbr A:";
-    cin >> a;
-    cout << " Enter Numbr B:";
-    cin >> b;
-
-    cout<<"Sum is:"<<a+b;
-
-    return 0;
+    cout<<"Hello World!";
 }
