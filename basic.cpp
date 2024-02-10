3ba7fa77301c0f3ae3828faf65555f014a6ba662
Sarika Talaviya <prof.rnwsarikaa1@gmail.com>
2024-02-10 14:56:01 +0530

    first file upload

---
+++ basic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ using namespace std;
+int main()
+{
+
+    int a, b;
+    cout << " Enter Numbr A:";
+    cin >> a;
+    cout << " Enter Numbr B:";
+    cin >> b;
+
+    cout<<"Sum is:"<<a+b;
+
+    return 0;
+}
